92258ee55e057f5cdaf99b50eae460b580c4a10e
Stephan Leicht Vogt <stephan@fanaka.ch>
2024-07-01 07:57:10 +0200

    fix macos key bindings in wezterm for word navigation

--- wezterm.lua
+++ wezterm.lua
@@ -23,16 +23,16 @@ config.window_background_opacity = 0.75
 config.macos_window_background_blur = 8
 
 config.keys = {
-  -- Clears the scrollback and viewport, and then sends CTRL-L to ask the
-  -- shell to redraw its prompt
-  {
-    key = 'K',
-    mods = 'SUPER',
-    action = act.Multiple {
-      act.ClearScrollback 'ScrollbackAndViewport',
-      act.SendKey { key = 'L', mods = 'CTRL' },
-    },
-  },
+  { mods = 'SUPER', key = 'K', action = act.Multiple { act.ClearScrollback 'ScrollbackAndViewport', act.SendKey { key = 'L', mods = 'CTRL' }, }, },
+  { mods = "OPT", key = "LeftArrow", action = act.SendKey({ mods = "ALT", key = "b" }) },
+  { mods = "OPT", key = "RightArrow", action = act.SendKey({ mods = "ALT", key = "f" }) },
+  { mods = "CMD", key = "LeftArrow", action = act.SendKey({ mods = "CTRL", key = "a" }) },
+  { mods = "CMD", key = "RightArrow", action = act.SendKey({ mods = "CTRL", key = "e" }) },
+  { mods = "CMD", key = "Backspace", action = act.SendKey({ mods = "CTRL", key = "u" }) },
+  { mods = "CMD|OPT", key = "LeftArrow", action = act.ActivateTabRelative(-1) },
+  { mods = "CMD|OPT", key = "RightArrow", action = act.ActivateTabRelative(1) },
+  { mods = "CMD|SHIFT", key = "LeftArrow", action = act.ActivateTabRelative(-1) },
+  { mods = "CMD|SHIFT", key = "RightArrow", action = act.ActivateTabRelative(1) },
 }
 
 -- and finally, return the configuration to wezterm
