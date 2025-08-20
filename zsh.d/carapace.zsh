f859429f7034b9772a4bc32ceb5bc1d6d1bfa4f0
Stephan Leicht Vogt <stephan@fanaka.ch>
2025-08-20 08:52:51 +0200

    update carapace config

--- zsh.d/carapace.zsh
+++ zsh.d/carapace.zsh
@@ -2,7 +2,7 @@
 
 # === multi-shell multi-command argument complete ===
 if command -v carapace > /dev/null; then
-  export CARAPACE_BRIDGES='zsh,fish,bash,inshellisense' # optional
+  export CARAPACE_BRIDGES='bat,zsh,fish,bash,inshellisense,tofu' # optional
   zstyle ':completion:*' format $'\e[2;37mCompleting %d\e[m'
   # shellcheck disable=SC1090
   source <(carapace _carapace)
