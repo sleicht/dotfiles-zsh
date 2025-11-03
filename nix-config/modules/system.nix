bfeaf4fc149d3556bc87436ad9baf32d257577b4
Stephan Leicht Vogt <stephan@fanaka.ch>
2025-11-03 21:46:05 +0100

    Comment out activationScripts.postUserActivation to disable automatic settings reload

--- nix-config/modules/system.nix
+++ nix-config/modules/system.nix
@@ -14,11 +14,11 @@
   system = {
     stateVersion = 5;
     # activationScripts are executed every time you boot the system or run `nixos-rebuild` / `darwin-rebuild`.
-    activationScripts.postUserActivation.text = ''
-      # activateSettings -u will reload the settings from the database and apply them to the current session,
-      # so we do not need to logout and login again to make the changes take effect.
-      /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
-    '';
+#    activationScripts.postUserActivation.text = ''
+#      # activateSettings -u will reload the settings from the database and apply them to the current session,
+#      # so we do not need to logout and login again to make the changes take effect.
+#      /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+#    '';
 
     primaryUser = "stephanlv_fanaka";
 
